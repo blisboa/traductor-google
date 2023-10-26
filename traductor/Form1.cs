072a3f31e51ee639c578db7d97a9db469d8dc206
bernardo lisboa <blisboa@gmail.com>
2023-10-26 13:45:58 -0300

    ejemplo  winform

---
+++ traductor/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace traductor
+{
+    public partial class Form1 : Form
+    {
+        GoogleTranslateApi.Language source;
+        GoogleTranslateApi.Language target;
+        GoogleTranslateApi.GoogleTranslator translate;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // definir idiomas 
+            source = GoogleTranslateApi.Language.Parse("es");
+            target = GoogleTranslateApi.Language.Parse("en");        
+
+            // declarar e inicializar Traductor
+            translate = new GoogleTranslateApi.GoogleTranslator(
+                source,
+                target
+            );
+
+            string textSource = "buenos dias";
+            string textTarget ="";
+
+            // el metodo Text del objeto translate es que traduce
+            textTarget =  translate.Text(textSource);
+
+            MessageBox.Show(textTarget);
+        }
+    }
+}
